190aeea0a9c9fb708398625d5a1d37988f1c5bd9
joaonrb <joaonrb@gmail.com>
2016-04-12 23:18:21 +0100

    Add caching to sessions

--- apps/sessions/middleware.go
+++ apps/sessions/middleware.go
@@ -9,41 +9,88 @@ import (
 	"lily"
 	lu "lily/utils"
 	"net/http"
+	"github.com/go-macaron/cache"
+	"strings"
+	"fmt"
 )
+
+var (
+	cacheEngine cache.Cache
+)
+
+func init() {
+	var err error
+	cacheEngine, err = cache.NewCacher("memory", cache.Options{})
+	if err != nil {
+		panic(err)
+	}
+}
 
 const (
 	DEFAULT_SESSION_COOKIE     = "LILYSESSION"
 	DEFAULT_SESSION_LENGTH     = 10
 	SESSION                    = "session"
+	DEFAULT_SESSION_TIMEOUT    = 12  // In hours
 )
 
 var (
-	cookieName   = DEFAULT_SESSION_COOKIE
-	cookieLength = DEFAULT_SESSION_LENGTH
+	cookieName    = DEFAULT_SESSION_COOKIE
+	cookieLength  = DEFAULT_SESSION_LENGTH
+	cookieTimeout = DEFAULT_SESSION_TIMEOUT
 )
 
 func init()  {
 	lily.RegisterMiddleware("sessions", Register)
 }
 
-func GetSession(request *lily.Request) string {
-	return request.Context[SESSION].(string)
+type session struct {
+	Cookie  string
+	session map[string]interface{}
+}
+
+func (self *session) Get(key string) interface{} {
+	self.load()
+	return self.session[key]
+}
+
+func (self *session) Set(key string, value interface{}) {
+	self.load()
+	self.session[key] = value
+}
+
+func (self *session) load() {
+	if self.session == nil {
+		if len(self.Cookie) != 0 {
+			tmp := cacheEngine.Get(fmt.Sprintf("%s_%s", SESSION, self.Cookie))
+			if tmp != nil {
+				self.session = tmp.(map[string]interface{})
+				return
+			}
+		}
+		self.session = make(map[string]interface{})
+	}
+}
+
+func GetSession(request *lily.Request) *session {
+	return request.Context[SESSION].(*session)
 }
 
 func CheckSession(request *lily.Request) {
-	request.Context[SESSION] = request.Header.Cookie(cookieName)
+	request.Context[SESSION] = &session{string(request.Header.Cookie(cookieName)), nil}
 }
 
 func SetSession(request *lily.Request, response *lily.Response) {
 	session := GetSession(request)
-	if _, exist := session[SESSION]; !exist {
+	if len(session.Cookie) == 0 {
+		session.Cookie = lu.GenerateBase64String(cookieLength)
 		cookie := &http.Cookie{
 			Name: cookieName,
-			Value: lu.GenerateBase64String(cookieLength),
+			Value: session.Cookie,
 			Path: "/",
 		}
 		response.Headers["Set-Cookie"] = cookie.String()
 	}
+	go cacheEngine.Put(session.Cookie, session.session, cookieTimeout * 60 * 60)
 }
 
 func Register(handler lily.IHandler) {
