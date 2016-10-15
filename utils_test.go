52cad641f700b702d6ff49e058d12afd13a1dd9a
joaonrb <joaonrb@gmail.com>
2016-10-15 04:24:23 +0100

    Iterator tests

--- utils_test.go
+++ utils_test.go
@@ -15,24 +15,35 @@ import (
 func TestLineIterator(t *testing.T) {
 	tmp := "/tmp/line_iterator.txt"
 	content := "line1\n1\n"
-	//defer os.Remove(tmp)
+	defer os.Remove(tmp)
 	err := ioutil.WriteFile(tmp, []byte(content), 0644)
 	if err != nil {
 		fmt.Printf("Tmp file couldn't be written becauser error %s", err.Error())
 		os.Exit(1)
 	}
 	iter, err := NewLineIterator(tmp)
+	defer iter.Close()
 	switch {
 	case err != nil:
 		t.Error(err.Error())
-	case iter.Next() != "line1" && iter.HasNext():
+	case iter.HasNext() && "line1" != iter.Next():
 		t.Error("Line 1 is not expected(line1).")
-	case iter.Next() != "1" && iter.HasNext():
+	case iter.HasNext() && iter.Next() != "1":
 		t.Error("Line 2 is not expected(1).")
-	case iter.Next() != "" && !iter.HasNext():
+	case iter.HasNext() && iter.Next() != "":
 		t.Error("Line 3 is not expected().")
-	case iter.Next() != "":
+	case iter.HasNext() && iter.Next() != "" && !iter.HasNext():
 		t.Error("Line 4 is not expected at all.")
+	}
+}
+
+func TestLineIteratorFileDontExist(t *testing.T) {
+	tmp := "/tmp/line_iterator_not.txt"
+	iter, err := NewLineIterator(tmp)
+	defer iter.Close()
+	switch {
+	case err == nil:
+		t.Error("Error do not happened")
 	}
 }
 
