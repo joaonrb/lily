621225d1c49601f9d9664842813145ac638baaa3
joaonrb <joaonrb@gmail.com>
2016-10-27 00:13:47 +0100

    Fix settings

--- settings.go
+++ settings.go
@@ -5,15 +5,44 @@
 package lily
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	SETTINGS_FILE       = "SERVER_SETTINGS"
+	defaultSettingsPath = "settings.yaml"
 )
 
 var Settings = map[string]interface{}{}
 
-func SetSettings(path string) error {
-	err := yaml.Unmarshal([]byte(path), Settings)
-	if err != nil {
-		return err
+func init() {
+	path := os.Getenv(SETTINGS_FILE)
+	if path == "" {
+		path = defaultSettingsPath
 	}
-	return nil
+
+	// Open file
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return
+	}
+	defer file.Close()
+
+	// Read file
+	setting, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return
+	}
+
+	// Read yaml to map
+	err = yaml.Unmarshal(setting, Settings)
+	if err != nil {
+		fmt.Printf("Error %s", err.Error())
+		return
+	}
 }
